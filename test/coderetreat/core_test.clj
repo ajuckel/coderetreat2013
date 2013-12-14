e555c025ca588f6300dfcbd98a053e62ae039930
Benjamin Rosas <ballPointPenguin@gmx.us>
2013-12-14 10:59:58 -0600

    is-alive-test

--- test/coderetreat/core_test.clj
+++ test/coderetreat/core_test.clj
@@ -16,3 +16,9 @@
 
 (deftest get-row-test
   (is [1 1 1] (get-row 2 [[0 0 0][1 1 1][0 0 0]])))
+
+(deftest get-first-cell-test
+  (is [1] (get-cell [0 0] [[1 0 0][0 0 0][0 0 0]])))
+
+(deftest check-if-alive-test
+  (is true (alive get-cell [0 0] [[1 0 0][0 0 0][0 0 0]])))
