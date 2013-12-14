019b39f12c7ab547e954081be0962ac11b3f8232
Anthony W. Juckel <ajuckel@gmail.com>
2013-12-14 10:21:58 -0600

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to coderetreat
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/coderetreat/core_test.clj
@@ -0,0 +1,7 @@
+(ns coderetreat.core-test
+  (:require [clojure.test :refer :all]
+            [coderetreat.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
