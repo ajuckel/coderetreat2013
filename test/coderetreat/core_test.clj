cb23ae5e7751ffd79fe990c4bd942b3ed300095c
Benjamin Rosas <ballPointPenguin@gmx.us>
2013-12-14 10:48:25 -0600

    test

--- test/coderetreat/core_test.clj
+++ test/coderetreat/core_test.clj
@@ -7,3 +7,9 @@
 
 (deftest empty-board-test
   (is [[0 0 0][0 0 0][0 0 0]] (board [[0 0 0][0 0 0][0 0 0]] )))
+
+(deftest lonley-board-test
+  (is [[0 0 0][0 1 0][0 0 0]] (board [[0 0 0][0 0 0][0 0 0]] )))
+
+(deftest a-day-on-the-empty-board-test
+  (is [[1 0 1][0 0 0][1 0 1]] (board [[0 0 0][0 0 0][0 0 0]] 1)))
